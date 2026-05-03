7c940d885eaf72c4422caa2cd6bee08928d45234
Prabhat-Kumar-Ahirwar <prabhatmmg007@gmail.com>
2026-05-03 11:50:23 +0530

    Initial commit

---
+++ src/test/java/com/example/PlantCare/PlantCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PlantCare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
