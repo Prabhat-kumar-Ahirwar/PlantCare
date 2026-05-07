3b531adf937f5f353c2b50f478b82cd5f60933ba
Prabhat-Kumar-Ahirwar <prabhatmmg007@gmail.com>
2026-05-07 13:47:35 +0530

    validation in Watering Schedule

--- src/main/java/com/example/PlantCare/model/WateringSchedule.java
+++ src/main/java/com/example/PlantCare/model/WateringSchedule.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -22,8 +23,16 @@ public class WateringSchedule {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull(message = "PlantId must be required")
+    @Positive(message = "PlantId must be positive")
     private Long plantId;
+
+    @Positive(message = "Frequency must be positive")
     private int frequencyInDays;
+
+    @PastOrPresent(message = "Last watered date cannot be future")
     private LocalDate lastWateredAt;
+
+    @FutureOrPresent(message = "Next watering date cannot be past")
     private LocalDate nextWateringAt;
 }
