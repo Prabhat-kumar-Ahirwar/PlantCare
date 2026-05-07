87590e16d78b107043ac1bc292b6f2abc203481c
Prabhat-Kumar-Ahirwar <prabhatmmg007@gmail.com>
2026-05-07 13:19:53 +0530

    validation in growthLog

--- src/main/java/com/example/PlantCare/model/GrowthLog.java
+++ src/main/java/com/example/PlantCare/model/GrowthLog.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,10 +20,18 @@ import java.time.LocalDate;
 public class GrowthLog{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
+    @NotNull(message = "Plant ID is required")
+    @Positive(message = "Plant ID must be positive")
     private Long plantId;
+
+    @NotNull(message = "Date is required")
+    @PastOrPresent(message = "Added date cannot be future date")
     private LocalDate date;
+
+    @NotNull(message = "Height is required")
+    @Positive(message = "Height must be positive")
     private Double heightCm;
+
+    @Size(max = 500, message = "Notes cannot exceed 500 characters")
     private String notes;
 }
