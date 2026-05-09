6500da105ef7c537ba84e76fdc8da2de500e8361
Prabhat-Kumar-Ahirwar <prabhatmmg007@gmail.com>
2026-05-09 23:08:51 +0530

    PlantDeletionException

---
+++ src/main/java/com/example/PlantCare/ExceptionHandler/plantDeletionException.java
@@ -0,0 +1,8 @@
+package com.example.PlantCare.ExceptionHandler;
+
+public class plantDeletionException extends RuntimeException {
+
+    public plantDeletionException(String message) {
+        super(message);
+    }
+}
